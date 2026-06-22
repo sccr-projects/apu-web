4126fa56b92e418dd70154731ffe8314721656f2
rif42 <rifkyariya42@gmail.com>
2026-06-22 10:52:59 +0700

    diaspora details fix checkpoint

--- src/data/diaspora.ts
+++ src/data/diaspora.ts
@@ -9,25 +9,19 @@ import profYayuk from "@/assets/images/academic/diaspora/prof-yayuk.webp";
 import mrDendi from "@/assets/images/academic/diaspora/mr-dendi.webp";
 import mrAgus from "@/assets/images/academic/diaspora/mr-agus.webp";
 import msEndah from "@/assets/images/academic/diaspora/ms-endah.webp";
-import { lecturers, sectionCopy } from "./lecturers";
-
-const baseLecturer = lecturers[0];
-const baseNotableResearch = sectionCopy.featured.notableResearch;
 
 export interface DiasporaPerson {
   id: string;
   name: string;
   role: string;
-  desc: string;
+  univ: string;
   href: string;
   image: { src: string };
   position: number;
   pastExperience: {
-    label: string;
     items: string[];
   };
   notableResearch: {
-    label: string;
     text: string;
   };
 }
@@ -36,203 +30,170 @@ export const people: DiasporaPerson[] = [
   {
     id: "faris",
     name: "Muhammad Faris, M.T.M.",
-    role: 'TBD',
-    desc: "Kirklareli University, Türkiye",
+    role: "TBD",
+    univ: "Kirklareli University, Türkiye",
     href: "#",
     image: mrFaris,
     position: 4,
     pastExperience: {
-      label: "TBD",
       items: ["TBD"],
     },
     notableResearch: {
-      label: "TBD",
       text: "TBD",
     },
   },
   {
     id: "adzani",
     name: "Adzani Gaisani Arda, BSc,MSc.",
-    role: 'TBD',
-    desc: "University of Debrecen, Hungary",
+    role: "TBD",
+    univ: "University of Debrecen, Hungary",
     href: "#",
     image: msAdzani,
     position: 12,
     pastExperience: {
-      label: "TBD",
       items: ["TBD"],
     },
     notableResearch: {
-      label: "TBD",
       text: "TBD",
     },
   },
   {
     id: "jutadi",
     name: "dr. Jutadi, MBBS",
-    role: 'TBD',
-    desc: "Changsha Medical University, China",
+    role: "TBD",
+    univ: "Changsha Medical University, China",
     href: "#",
     image: mrJutadi,
     position: 20,
     pastExperience: {
-      label: "TBD",
       items: ["TBD"],
     },
     notableResearch: {
-      label: "TBD",
       text: "TBD",
     },
   },
   {
     id: "dina",
     name: "Nur Dina A.M.Sc., (Ph.D. cand)",
-    role: 'TBD',
-    desc: "NAIST Japan",
+    role: "TBD",
+    univ: "NAIST Japan",
     href: "#",
     image: msDina,
     position: 28,
     pastExperience: {
-      label: "TBD",
       items: ["TBD"],
     },
     notableResearch: {
-      label: "TBD",
       text: "TBD",
     },
   },
   {
     id: "faheem",
     name: "Dr. Faheem Ahmed Khan, B.Sc, M.S, Ph.D",
-    role: 'TBD',
-    desc: "M.I.T. USA",
+    role: "TBD",
+    univ: "M.I.T. USA",
     href: "#",
     image: mrFaheem,
     position: 37,
     pastExperience: {
-      label: "TBD",
       items: ["TBD"],
     },
     notableResearch: {
-      label: "TBD",
       text: "TBD",
     },
   },
   {
     id: "prof-agung",
     name: "Prof. Dr. dr. Agung Putra, M.Si. Med.",
-    role: 'TBD',
-    desc: "Founder & Commissioner SCCR Indonesia",
+    role: "Founder & Commissioner SCCR Indonesia",
+    univ: "",
     href: "#",
     image: profAgung,
     position: 50,
     pastExperience: {
-      label: "TBD",
-      items: ["TBD"],
+      items: [
+        "Ph.D. in Constitutional Law, University of Melbourne",
+        "Visiting Scholar, Max Planck Institute",
+        "Senior Legal Advisor, ASEAN Secretariat",
+      ],
     },
     notableResearch: {
-      label: "TBD",
-      text: "TBD",
+      text: "Leading comparative research on constitutional reform and human-rights frameworks in Southeast Asia, with published work on judicial independence and democratic transitions.",
     },
   },
   {
     id: "waheni",
     name: "Waheni Rizki Aprilia, S.Si., Ph.D.",
-    role: 'TBD',
-    desc: "Suranaree University of Technology, Thailand",
+    role: "TBD",
+    univ: "Suranaree University of Technology, Thailand",
     href: "#",
     image: msWaheni,
     position: 62,
     pastExperience: {
-      label: "TBD",
       items: ["TBD"],
     },
     notableResearch: {
-      label: "TBD",
       text: "TBD",
     },
   },
   {
     id: "yayuk",
     name: "Prof. Yayuk Astuti, S.Si, Ph.D",
-    role: 'TBD',
-    desc: "Newcastle University, UK",
+    role: "TBD",
+    univ: "Newcastle University, UK",
     href: "#",
     image: profYayuk,
     position: 71,
     pastExperience: {
-      label: "TBD",
       items: ["TBD"],
     },
     notableResearch: {
-      label: "TBD",
       text: "TBD",
     },
   },
   {
     id: "dendi",
     name: "Dendi Krisna Nugraha, M.Sc., Ph.D.",
-    role: 'TBD',
-    desc: "RIMD, Osaka Japan",
+    role: "TBD",
+    univ: "RIMD, Osaka Japan",
     href: "#",
     image: mrDendi,
     position: 79,
     pastExperience: {
-      label: "TBD",
       items: ["TBD"],
     },
     notableResearch: {
-      label: "TBD",
       text: "TBD",
     },
   },
   {
     id: "agus",
     name: "Dr. Ir. Agus F. Abdillah, MBA",
-    role: 'TBD',
-    desc: "University of Birmingham, UK",
+    role: "TBD",
+    univ: "University of Birmingham, UK",
     href: "#",
     image: mrAgus,
     position: 87,
     pastExperience: {
-      label: "TBD",
       items: ["TBD"],
     },
     notableResearch: {
-      label: "TBD",
       text: "TBD",
     },
   },
   {
     id: "endah",
     name: "Endah Agustina Lestari BSc,MSc.",
-    role: 'TBD',
-    desc: "University of Queensland, Australia",
+    role: "TBD",
+    univ: "University of Queensland, Australia",
     href: "#",
     image: msEndah,
     position: 96,
     pastExperience: {
-      label: "TBD",
       items: ["TBD"],
     },
     notableResearch: {
-      label: "TBD",
       text: "TBD",
     },
   },
 ];
-
-export const diasporaLecturer = {
-  id: baseLecturer.id,
-  name: baseLecturer.name,
-  role: 'TBD',
-  image: profAgung,
-  pastExperience: {
-    label: "TBD",
-    items: ["TBD"],
-  },
-  notableResearch: {
-    label: "TBD",
-    text: "TBD",
-  },
-};
